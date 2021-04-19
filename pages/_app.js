0be037f3dccbfec991f716e2be5273d368ea0629
Christian Alares <christian.alares@gmail.com>
2021-04-19 17:03:31 +0200

    Add google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
 import { useRef } from 'react'
 import Head from 'next/head'
-// import { createGlobalStyle } from 'styled-components'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Hydrate } from 'react-query/hydration'
 import GlobalStyles from '../styles/GlobalStyles'
-// import '../styles/globals.scss'
-
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     background: red !important;
-//   }
-// `
 
 function MyApp({ Component, pageProps }) {
   const queryClientRef = useRef()
@@ -24,11 +16,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Värmex.se</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,500;0,700;1,500&display=swap"
-        />
       </Head>
       <QueryClientProvider client={queryClientRef.current}>
         <Hydrate state={pageProps.dehydratedState}>
--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components'
 import { mqw, space, fonts } from './utils'
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,500;0,700;1,500&display=swap');
+
 :root {
   --header-height: 72px;
 
